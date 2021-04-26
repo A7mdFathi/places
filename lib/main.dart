bd2b5f6e0eb9e11bad5624141f56f4f2e4f8c4c5
Ahmed <a.fathi2112@gmail.com>
2021-04-26 17:38:45 +0200

    Place list & place provider

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:places/providers/great_places.dart';
+import 'package:places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPLaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PLacesListScreen(),
+      ),
+    );
+  }
+}
