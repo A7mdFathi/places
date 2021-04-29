f4278a3ac40efa160dd790e91fe4fe38e74aabd0
Ahmed M <sdfdsd>
2021-04-29 11:37:59 +0200

    storing and fetching data with sql

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -20,22 +20,32 @@ class PLacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        builder: (context, places, ch) => places.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemBuilder: (context, index) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(places.items[index].image),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatPlaces>(
+                    builder: (context, places, ch) => places.items.length <= 0
+                        ? ch
+                        : ListView.builder(
+                            itemBuilder: (context, index) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(places.items[index].image),
+                              ),
+                              title: Text(places.items[index].title),
+                              onTap: null,
+                            ),
+                            itemCount: places.items.length,
+                          ),
+                    child: Center(
+                      child: const Text('There are no places'),
+                    ),
                   ),
-                  title: Text(places.items[index].title),
-                  onTap: null,
-                ),
-                itemCount: places.items.length,
-              ),
-        child: Center(
-          child: const Text('There are no places'),
-        ),
       ),
     );
   }
